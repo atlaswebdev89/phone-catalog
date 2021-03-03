fa6deab6c1439b9ec3c288922e985e5a67de2606
Atlas <doroshuk33@yandex.by>
2021-03-03 10:02:43 +0300

    update

--- child-themes/functions.php
+++ child-themes/functions.php
@@ -1,6 +1,34 @@
 <?php 
+
 require_once( get_stylesheet_directory(). "/includes/front-page.php" );	// Landing Page outputs
 require_once( get_stylesheet_directory(). "/includes/categoryList.php" );// Для страниц рубрик вывод категорий и записей
+
+/**
+ * Delete meta data from headers
+ */
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links',2);
+remove_action('wp_head', 'feed_links_extra',3);
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+remove_action('wp_head', 'wp_shortlink_wp_head',10,0);
+remove_action('wp_head', 'wp_oembed_add_discovery_links' );
+remove_action('wp_head', 'wp_oembed_add_host_js');
+// Удаляем информацию о REST API из заголовков HTTP и секции head
+remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+//dns-prefetch
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
+//Emoji из WordPress
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+//Нужен он для редактора Gutenberg
+add_action( 'wp_enqueue_scripts', 'wpassist_remove_block_library_css' );
+function wpassist_remove_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+}
 
 //Подключение стилей родетельской темы FLUID
 add_action( 'wp_enqueue_scripts', 'true_enqueue_styles' );
